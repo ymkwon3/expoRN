e5deeff29b84551877afbb9cc945aa1a797e013f
youngmin <ymkwon3@gnu.ac.kr>
2022-07-15 00:49:18 +0900

    weather api

--- App.js
+++ App.js
@@ -1,73 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, ScrollView, Text, View, Dimensions} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
-const {width: SCREEN_WIDTH} = Dimensions.get('window');
-console.log(SCREEN_WIDTH)
+import {
+  StyleSheet,
+  ScrollView,
+  Text,
+  View,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+// not safe
+const API_KEY = "";
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const {
+      [0]: { country, region, city },
+    } = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+
+    setCity(`${country} ${region} ${city}`);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>위치</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
-      <ScrollView 
-      contentContainerStyle={styles.weather} 
-      showsHorizontalScrollIndicator={false}
-      horizontal 
-      pagingEnabled
+      <ScrollView
+        contentContainerStyle={styles.weather}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        pagingEnabled
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>온도</Text>
-          <Text style={styles.description}>날씨</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>온도</Text>
-          <Text style={styles.description}>날씨</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>온도</Text>
-          <Text style={styles.description}>날씨</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>온도</Text>
-          <Text style={styles.description}>날씨</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>온도</Text>
-          <Text style={styles.description}>날씨</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" size="large"></ActivityIndicator>
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View style={styles.day} key={index}>
+              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(0)}</Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
-      <StatusBar ></StatusBar>
+      <StatusBar></StatusBar>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    backgroundColor: 'tomato'
+    flex: 1,
+    backgroundColor: "tomato",
   },
   city: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   cityName: {
-    fontSize: 48,
-    fontWeight: '600',
+    fontSize: 30,
+    fontWeight: "600",
   },
-  weather: {
-  },
+  weather: {},
   day: {
-    width : SCREEN_WIDTH,
-    alignItems: 'center',
+    width: SCREEN_WIDTH,
+    alignItems: "center",
   },
   temp: {
     marginTop: 50,
     fontSize: 68,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   description: {
-    fontSize: 38
+    fontSize: 38,
   },
+  tinyText: {
+    fontSize: 18
+  }
 });
